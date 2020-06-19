7c68005408cab7352afcc8c8813956de8dff169d
Samuel Huebner <samuelhuebner@Samuels-MBP.ber.axonetiq.com>
2020-06-19 17:29:40 +0200

    Moves config to .env

---
+++ config/config.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    operatorsAliases: 0
+    // "logging": process.env.LOGGING
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT,
+    storage: process.env.STORAGE,
+    operatorsAliases: 0,
+    logging: false,
+    debug: 0
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    operatorsAliases: 0
+  }
+};
