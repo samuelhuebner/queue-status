14088ccc55af0d286c465315538c09e0d58e0dcb
Samuel Huebner <samuelhuebner@Samuels-MBP.ber.axonetiq.com>
2020-06-22 08:59:36 +0200

    Adds query auth for voys

--- middleware/auth.mw.js
+++ middleware/auth.mw.js
@@ -1,5 +1,7 @@
+const _ = require('lodash');
+
 module.exports = (req, res, next) => {
-    if (req.method === 'POST' && !req.ip.includes(process.env.ALLOWED_POST_IP)) {
+    if (req.method === 'POST' && _.get(req.query, 'fred') !== process.env.API_KEY  || !req.ip.includes(process.env.ALLOWED_POST_IP)) {
         res.status(403).send('you are not authorized to post to this url');
     } else {
         next();
