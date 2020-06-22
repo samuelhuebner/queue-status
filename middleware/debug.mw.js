5cdd06fa12f1b22044db9328684a55ee29706eeb
Samuel Huebner <samuelhuebner@Samuels-MBP.ber.axonetiq.com>
2020-06-22 09:33:05 +0200

    Adds more logging to debugging

--- middleware/debug.mw.js
+++ middleware/debug.mw.js
@@ -2,7 +2,8 @@
 module.exports = (req, res, next) => {
     if (process.env.DEBUG) {
         console.log(`${req.method}: ${req.originalUrl}`);
-        console.log(`user-id: ${req.get('user-id')}`);
+        console.log(`call-id: ${req.get('call_id')}`);
+        console.log(`status: ${req.get('status')}`);
     }
     next();
 };
