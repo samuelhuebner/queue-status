76d2e256320b282011cc9e61f91416defcd0a0fb
Samuel Huebner <samuelhuebner@Samuels-MBP.ber.axonetiq.com>
2020-06-22 09:34:57 +0200

    Fixes logging

--- middleware/debug.mw.js
+++ middleware/debug.mw.js
@@ -1,9 +1,10 @@
+const _ = require('lodash');
 // enables debugging logs if flag is set
 module.exports = (req, res, next) => {
     if (process.env.DEBUG) {
         console.log(`${req.method}: ${req.originalUrl}`);
-        console.log(`call-id: ${req.get('call_id')}`);
-        console.log(`status: ${req.get('status')}`);
+        console.log(`call-id: ${_.get(req.body, 'call_id')}`);
+        console.log(`status: ${_.get(req.body, 'status')}`);
     }
     next();
 };
