42e613bb403bc2532ce9f2170fe3d5211a897b1a
Samuel Huebner <whit3shark@Samuels-MBP.home>
2020-07-29 04:16:51 +0200

    - Adds function to get the call count of all inbound calls in a month

--- controller/index.js
+++ controller/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     CallProcessingController: require('./call-processing.controller'),
-    QueueInfoController: require('./queue-info.controller')
+    QueueInfoController: require('./queue-info.controller'),
+    CallInformationController: require('./call-information.controller')
 }
