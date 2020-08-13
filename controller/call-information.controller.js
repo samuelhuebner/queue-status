b43d1843ff4780f165281b28d5ee7b38160632ea
Samuel Huebner <whit3shark@whit3shark.local>
2020-08-13 16:53:31 +0200

    // WIP

--- controller/call-information.controller.js
+++ controller/call-information.controller.js
@@ -3,18 +3,48 @@ const { Op } = require('sequelize');
 
 class CallInformationController {
 
-    getInboundCallNumberMonth() {
-        var date = new Date(), y = date.getFullYear(), m = date.getMonth();
-        var firstDay = new Date(y, m, 1);
-        var lastDay = new Date(y, m + 1, 0);
+    getInboundCallNumber(startDate, endDate) {
+        var date, firstDay, lastDay, y, m;
+
+        if (!startDate) {
+            date = new Date(), y = date.getFullYear(), m = date.getMonth();
+            firstDay = new Date(y, m, 1);
+        } else {
+            date = startDate;
+            y = date.getFullYear();
+            m = date.getMonth();
+
+            firstDay = new Date(y, m, date.getDate());
+        }
+
+        lastDay = endDate || new Date(y, m + 1, 0);
 
         return db.callInitiation.count({
             where: {
                 callInitiationTime: {
-                    [Op.between]: [firstDay, lastDay] 
+                    [Op.between]: [firstDay, lastDay]
                 }
             }
         });
+    }
+
+    async getReachability(start = new Date(), end = new Date()) {
+        start.setHours(0, 0, 0, 0);
+        end.setHours(23, 59, 59, 59);
+
+        var callInitCount;
+        
+        const timestring = `${start.getDate()}-${start.getMonth()}-${start.getFullYear()}`
+        callInitCount = await db.callInitiation.count({
+            where: {
+                callInitiationTime: {
+                    [Op.between]: [start, end]
+                }
+            }
+        })
+
+
+        return { calls: callInitCount };
     }
 
 }
--- routes/web-access.routes.js
+++ routes/web-access.routes.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const { Router } = require('express');
 const { QueueInfoController, CallInformationController } = require('../controller');
 
@@ -11,6 +12,7 @@ class WebAccessRoutes {
         this.router.get('/queue-status', this.getStatus.bind(this));
         this.router.get('/queue-status/event-stream', this.getEventStream.bind(this));
         this.router.get('/call-stats/monthly-inbound', this.getMonthlyInboundCallCount.bind(this));
+        this.router.get('/call-stats/daily-reachability', this.getDailyInboundReachability.bind(this));
     }
 
     getStatus(req, res, next) {
@@ -20,9 +22,21 @@ class WebAccessRoutes {
     }
 
     async getMonthlyInboundCallCount(req, res, next) {
-        const count = await this.callInfoController.getInboundCallNumberMonth();
+        const count = await this.callInfoController.getInboundCallNumber();
         res.set('Access-Control-Allow-Origin', '*') // TODO: allow only frontend url 
         res.status(200).send({ count });
+    }
+
+    async getDailyInboundReachability(req, res, next) {
+        const day = _.get(req.query, 'day');
+
+        let count;
+        if (day) {
+            count = await this.callInfoController.getReachability(new Date(day), new Date(day));
+        } else {
+            count = await this.callInfoController.getReachability();
+        }
+        res.status(200).send({ reachability: 100, numberOfCalls: count });
     }
 
     getEventStream(req, res, next) {
