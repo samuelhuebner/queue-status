be21425a65929748c07fa6506da5921874ff7b43
whit3shark <samu.huebner@gmail.com>
2020-10-10 16:50:23 +0200

    - Adds user-model to database

---
+++ models/user.model.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+    const user = sequelize.define('user', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    });
+
+    return user;
+};
