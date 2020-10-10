aa09d99c3f9c3c8be5fecd837e57fd53c93b8b38
whit3shark <samu.huebner@gmail.com>
2020-10-10 16:51:49 +0200

    - Adds jwt authentication

--- middleware/auth.mw.js
+++ middleware/auth.mw.js
@@ -1,6 +1,11 @@
 const _ = require('lodash');
 
 module.exports = (req, res, next) => {
+    if (req.originalUrl === '/auth/signup' || req.originalUrl === '/auth/login') {
+        next();
+        return;
+    }
+
     if (req.method === 'POST' && _.get(req.query, 'fred') !== process.env.API_KEY) {
         res.status(403).send('you are not authorized to post to this url');
         if (process.env.DEBUG) {
