411f785856412c15ab49004e5be00e2a2ee5c8b1
whit3shark <samu.huebner@gmail.com>
2020-10-15 21:28:45 +0200

    - Adds ongoing-call-service

---
+++ services/ongoing-call.service.js
@@ -0,0 +1,55 @@
+const event = require('./event.service');
+
+class OngoingCallService {
+    constructor() {
+        this.ongoingCalls = [];
+    }
+
+    getOngoingCallsList() {
+        return this.ongoingCalls;
+    }
+
+    addNewCall(call) {
+        this.ongoingCalls.unshift(call);
+    }
+
+    updateExistingCall(call) {
+        this.ongoingCalls.forEach((item, index) => {
+            if (item.callId !== call.callId) {
+                return;
+            }
+
+            this.ongoingCalls[index] = { ...item, call };
+
+            event.emit('newCallInProgress', call.callId);
+        });
+    }
+
+    removeOngoingCall(call) {
+        this.ongoingCalls.forEach((item, index) => {
+            if (item.callId !== call.callId) {
+                return;
+            }
+
+            const [deleted] = this.ongoingCalls.splice(index, 1);
+            if (deleted) {
+                event.emit('callFinished', deleted.callId);
+            }
+        });
+    }
+
+    getCall(callId) {
+        let returnValue = {};
+        this.ongoingCalls.forEach((item) => {
+            if (item.callId !== callId) {
+                return;
+            }
+
+            returnValue = item;
+        });
+
+        return returnValue;
+    }
+}
+
+module.exports = new OngoingCallService();
