ffd9c264ffa3ddde26fdf6db176840dece3406b0
whit3shark <samu.huebner@gmail.com>
2020-10-15 21:29:29 +0200

    - removes debug output

--- middleware/error.mw.js
+++ middleware/error.mw.js
@@ -3,7 +3,6 @@
 module.exports = (err, req, res, next) => {
     const { status = 500 } = err;
 
-    console.log('hi from the error handler');
     res.setHeader('Content-Type', 'application/json');
     res.status(status).send({ error: err });
 };
