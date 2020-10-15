2c167cd8e211dbd155d8cc80f6d2972f01ae66d6
whit3shark <samu.huebner@gmail.com>
2020-10-15 14:30:12 +0200

    - Adds option to socket io

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ class Server {
         this.app.listen(process.env.SVC_PORT, () => console.log(`${process.env.SVC_NAME} listening to port ${process.env.SVC_PORT}`));
 
         this.http = require('http').Server(this.app);
-        this.io = require('socket.io')(this.http, { path: '/websocket/socket.io' });
+        this.io = require('socket.io')(this.http, { path: '/websocket/socket.io', transport: ['websocket'] });
 
         this.io.on('connection', (socket) => {
             console.log('client connected');
