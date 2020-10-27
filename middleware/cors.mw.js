33fc3f9023bc47ef5828e6f182996a525c0edd13
Samuel Hübner <samu.huebner@gmail.com>
2020-10-27 11:47:35 +0100

    - Adds DELETE to allowed cors methods

--- middleware/cors.mw.js
+++ middleware/cors.mw.js
@@ -1,5 +1,6 @@
 module.exports = (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, user-id');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE');
     next();
 };
